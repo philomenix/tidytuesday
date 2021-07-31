468a33a4e12dd2d0b511a23517afe3e22cced0f0
Meander Sips <meandersips@gmail.com>
2021-07-31 11:45:57 +0200

    update week 31

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Tidy Tuesday
-### 2021
+## 2021
+### Week 31
 
-[2021 - Week 29 - Scooby Doo](https://philomenix.github.io/tidytuesday/2021_29/index.html)
+[Code](https://philomenix.github.io/tidytuesday/2021_31/index.html)
+
+![Week 31 2021](./result/2021_31_1.png)
---
+++ code/helpers/helpers.R
@@ -0,0 +1,44 @@
+# Unit Conversion ----
+
+# Reminder
+  # 1 pt = 1/72 inch (physical size)
+  # 1 px = 1/96 inch at 96 ppi (non-physical size)
+  # 1 px = 0.75 pt at 96 ppi (default)
+  # 1 px = 1 pt at 72 ppi
+
+# pixels <-> inches
+px2in <- function(px, ppi = 96) { px / ppi }
+in2px <- function(i, ppi = 96) { i * ppi }
+
+# pixels <-> points
+px2pt <- function(px, ppi = 96) { px * (72 / ppi) }
+pt2px <- function(pt, ppi = 96) { round(pt / (72 / ppi)) }
+
+# pixels <-> millimeters
+px2mm <- function(px, ppi = 96) { px / ppi * 25.4 }
+mm2px <- function(mm, ppi = 96) { mm / 25.4 * ppi }
+
+# points <-> inches
+pt2in <- function(pt) { pt * (1 / 72) }
+in2pt <- function(i) { i * 72 }
+
+# points <-> millimeters
+pt2mm <- function(pt) { pt * (1 / 72) * 25.4 }
+mm2pt <- function(mm) { (mm / 25.4) * 72 }
+
+# inches <-> millimeters
+in2mm <- function(i) { i * 25.4 }
+mm2in <- function(mm) { mm / 25.4 }
+
+# Color Manipulation ----
+
+# lengthen/shorten color palette (vector)
+pal_length <- function(colors, n){
+  ramp <- colorRamp(colors)
+  hex <- character()
+  for(t in seq(0, 1, length.out = n)) {
+    hex <- c(hex, tolower(rgb(ramp(t), maxColorValue = 255)))
+  }
+  hex
+}
+
